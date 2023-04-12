564ec73916947bb99c263f25c54c14e281f4252a
ntordomar <ntordomar@itba.edu.ar>
2023-04-12 11:17:26 -0300

    Connection with workers stablished

--- application.c
+++ application.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "application.h"
 #include <sys/stat.h>
+#include <string.h>
 
 //PREGUNTAS
 // SI TIENE QUE SER VARIABLE LA CANTIDAD DE SLAVES
@@ -63,40 +64,44 @@ int main(int argc, char *argv[]) {
 
         //Closing FD from child and changing stdin and stdout
         if(worker_fork == CHILD) {
+            int j;
+            for(j=0; j<i;j++){
+                close(workers_fds[READ][j]);
+                close(workers_fds[WRITE][j]);
+            }
             close(0); // From now on, recieving information from the pipe instead od stdin
-            dup(pipe_files[0]);
+            dup(pipe_files[READ]);
             close(1); // form now on, sending information to the pipe instead of stdout
-            dup(pipe_data[1]);
-            close(pipe_files[0]);
-            close(pipe_data[1]);
-            close(pipe_data[0]);
-            close(pipe_files[1]);
+            dup(pipe_data[WRITE]);
+            close(pipe_files[READ]);
+            close(pipe_data[WRITE]);
+            close(pipe_data[READ]);
+            close(pipe_files[WRITE]);
             execve("./worker", worker_parameters, 0);
         }
         
-        close(pipe_files[0]); //Closing pipes ends that the proccess is not going to use.
-        close(pipe_data[1]);
+        close(pipe_files[READ]); //Closing pipes ends that the proccess is not going to use.
+        close(pipe_data[WRITE]);
     }
 
-   
-        
+    printf("Cantidad de workers: %d\n", num_workers);
+    
     //INICIALMENTE MANDAMOS A CADA UNO DE LOS WORKERS 1 TRABAJO
     int file_to_send;
     for(file_to_send = 0; file_to_send < num_workers; file_to_send++) {
-        write(workers_fds[WRITE][file_to_send], files_paths[file_to_send],300);
+       if( write(workers_fds[WRITE][file_to_send], files_paths[file_to_send], strlen(files_paths[file_to_send])) == -1) {
+            error_call("Write failed", 1);
+       };
+    
     }
     int j;
     char buffer [300] = {0};
     for(j = 0; j < num_workers; j++) {
-        read(workers_fds[READ][j], buffer, 300);
+        int n = read(workers_fds[READ][j],buffer,300 );
+        buffer[n]=0;
         printf("%s \n",buffer);
     }
 
-    
-
-    //SELECT ETC ETC
-    //Nosotros vamos a estar haciendo reads de pipe_data[0]
-    
     return 0;
 }
 
--- worker.c
+++ worker.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 int main(int argc, char const *argv[]) {
-   char buff[300];
-   read(0, buff, 300);
+   char buff[300] = {0};
+   int n = read(0, buff, 300);
+   buff[n]=0;
    write(1, buff, 300);
    return 0;
 }
